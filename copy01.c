96bbf9ee36ba25fabfc2f18ccfaf69c27ad26194
wanglinzhuo w00510727 <wanglingzhuo@huawei.com>
2020-03-17 20:13:29 +0800

    Replace context with ctx

--- copy01.c
+++ copy01.c
@@ -1,32 +1,32 @@
 /*
- * This function allocates an e2fsck context
+ * This function allocates an e2fsck ctx
  */
-errcode_t e2fsck_allocate_context(e2fsck_t *ret)
+errcode_t e2fsck_allocate_ctx(e2fsck_t *ret)
 {
-	e2fsck_t	context;
+	e2fsck_t	ctx;
 	errcode_t	retval;
 	char		*time_env;
 
-	retval = ext2fs_get_mem(sizeof(struct e2fsck_struct), &context);
+	retval = ext2fs_get_mem(sizeof(struct e2fsck_struct), &ctx);
 	if (retval)
 		return retval;
 
-	memset(context, 0, sizeof(struct e2fsck_struct));
+	memset(ctx, 0, sizeof(struct e2fsck_struct));
 
-	context->process_inode_size = 256;
-	context->ext_attr_ver = 2;
-	context->blocks_per_page = 1;
-	context->htree_slack_percentage = 255;
+	ctx->process_inode_size = 256;
+	ctx->ext_attr_ver = 2;
+	ctx->blocks_per_page = 1;
+	ctx->htree_slack_percentage = 255;
 
 	time_env = getenv("E2FSCK_TIME");
 	if (time_env)
-		context->now = (time_t) strtoull(time_env, NULL, 0);
+		ctx->now = (time_t) strtoull(time_env, NULL, 0);
 	else {
-		context->now = time(0);
-		if (context->now < 1262322000) /* January 1 2010 */
-			context->flags |= E2F_FLAG_TIME_INSANE;
+		ctx->now = time(0);
+		if (ctx->now < 1262322000) /* January 1 2010 */
+			ctx->flags |= E2F_FLAG_TIME_INSANE;
 	}
 
-	*ret = context;
+	*ret = ctx;
 	return 0;
 }
